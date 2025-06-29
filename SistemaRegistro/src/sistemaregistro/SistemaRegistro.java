1e460f207254cbac5bbc65cf3440d610ae4dd2bc
Gian0709 <gfcobross@gmail.com>
2025-06-28 19:10:20 -0500

    Add files via upload

---
+++ SistemaRegistro/src/sistemaregistro/SistemaRegistro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemaregistro;
+
+import sistemaregistro.admin.Acceso;
+
+/**
+ *
+ * @author hidea
+ */
+public class SistemaRegistro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Acceso x = new Acceso();
+        x.setVisible(true);
+    }
+    
+}
